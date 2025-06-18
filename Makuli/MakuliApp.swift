407be32cb918712b6810e58858a1ec908e5b3728
Ian Kiprotich <kiprotichian254@gmail.com>
2025-06-18 12:17:25 +0300

    Initial Makuli project setup

---
+++ Makuli/MakuliApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakuliApp.swift
+//  Makuli
+//
+//  Created by Ian   on 18/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MakuliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
