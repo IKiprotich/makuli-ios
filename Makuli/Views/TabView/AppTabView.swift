a45a058d5691083d1e8e76a659e69c22a5e14908
Ian Kiprotich <kiprotichian254@gmail.com>
2025-06-23 16:35:56 +0300

    Ensure AppTabView passes selectedTab binding to HomeView

--- Makuli/Views/TabView/AppTabView.swift
+++ Makuli/Views/TabView/AppTabView.swift
@@ -7,28 +7,36 @@
 
 import SwiftUI
 
+
 struct AppTabView: View {
+    
+    @State private var selectedTab = 0
+    
     var body: some View {
-        TabView {
-            HomeView()
+        TabView(selection: $selectedTab) {
+            HomeView(selectedTab: $selectedTab)
                 .tabItem {
                     Label("Home", systemImage: "house")
                 }
+                .tag(0)
 
             PlansView()
                 .tabItem {
                     Label("Plans", systemImage: "calendar")
                 }
+                .tag(1)
 
             RecipesView()
                 .tabItem {
                     Label("Recipes", systemImage: "fork.knife")
                 }
+                .tag(2)
 
             ProfileView()
                 .tabItem {
                     Label("Profile", systemImage: "person.crop.circle")
                 }
+                .tag(3)
         }
         .accentColor(AppColors.primaryOrange)
     }
